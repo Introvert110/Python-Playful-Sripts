4cd3bef1c48c0bedc244c711df89d88991cdf7e6
Mohd Rizwanul Hasan <114355006+Introvert110@users.noreply.github.com>
2023-06-18 14:31:42 +0530

    Add files via upload

---
+++ What_is_the_Time.py
@@ -0,0 +1,15 @@
+import time
+t = time.strftime('%H:%M:%S')
+hour = int(time.strftime('%H'))
+#print(hour)
+
+if(hour>=0 and hour<12):
+    print("Good Morning Sir")
+elif(hour>=12 and hour<16):
+    print("Good Afternoon Sir!")
+elif(hour>=16 and hour<20):
+    print("Good Evening Sir!")
+elif(hour>=20 and hour<0):
+    print("Good Night Sir!")
+
+
